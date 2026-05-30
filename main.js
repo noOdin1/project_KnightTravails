5d0a96ae464b5bedb3c1f971885219cafadd9277
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-05-30 23:19:42 +0800

    Imports to run the knightsTravails program

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /* main.js */
-import { board } from "./knightTravails.js";
+import { board, dirTranslation, mapBoard } from "./knightTravails.js";
+import { Tree } from "./binary_search_tree.js";
 
 board.knightMoves([0, 0], [7, 7]);
 board.knightMoves([3, 3], [0, 0]);
