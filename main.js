cb2e341672f7796282698622d61c4e5ce8527d6d
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-06-07 03:04:42 +0800

    Get the first and second argument from cli

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ import { Tree } from "./binary_search_tree.js";
 import { argv } from "node:process";
 
 (() => {
+  // get the argument from console via 'argv' (of node:process)
+  //  and start calculating all the moves
+  // Use the following command:
+  //  node main.js [3,2] [5,7]
+  let startPos = JSON.parse(argv[2]);
+  let destPos = JSON.parse(argv[3]);
 
 
 })();
