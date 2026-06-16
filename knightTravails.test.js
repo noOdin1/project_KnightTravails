1f55702c2b60ce2440f5b87cfb9f344e67a61b93
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-06-16 16:33:25 +0800

    Updating validPos after 'board' rename

--- knightTravails.test.js
+++ knightTravails.test.js
@@ -77,60 +77,66 @@ describe("Test group for getAngle())", () => {
   });
 });
 
-describe("validPos() test group", () => {
+describe.only("validPos() test group", () => {
+  let tk;
+  beforeEach(() => {
+    // before each test, reset tmpList to a new empty Hash Set
+    tk = travellingKnight();
+  });
+
   test("No arg passed in", () => {
-    expect(board.validPos()).toEqual(undefined);
+    expect(tk.validPos()).toEqual(undefined);
   });
   test("Non array passed in", () => {
-    expect(board.validPos(5)).toEqual(undefined);
+    expect(tk.validPos(5)).toEqual(undefined);
   });
   test("Array passed in, edge case [0, 0]", () => {
-    expect(board.validPos([0, 0])).toEqual(true);
+    expect(tk.validPos([0, 0])).toEqual(true);
   });
   test("Array passed in, edge case [7, 0]", () => {
-    expect(board.validPos([7, 0])).toEqual(true);
+    expect(tk.validPos([7, 0])).toEqual(true);
   });
   test("Array passed in, edge case [0, 7]", () => {
-    expect(board.validPos([0, 7])).toEqual(true);
+    expect(tk.validPos([0, 7])).toEqual(true);
   });
   test("Array passed in, edge case [7, 7]", () => {
-    expect(board.validPos([7, 7])).toEqual(true);
+    expect(tk.validPos([7, 7])).toEqual(true);
   });
   test("Array passed in, edge case [0, -1]", () => {
-    expect(board.validPos([0, -1])).toEqual(false);
+    expect(tk.validPos([0, -1])).toEqual(false);
   });
   test("Array passed in, edge case [-1, 0]", () => {
-    expect(board.validPos([-1, 0])).toEqual(false);
+    expect(tk.validPos([-1, 0])).toEqual(false);
   });
   test("Array passed in, edge case [-1, -1]", () => {
-    expect(board.validPos([-1, -1])).toEqual(false);
+    expect(tk.validPos([-1, -1])).toEqual(false);
   });
   test("Array passed in, edge case [0, 8]", () => {
-    expect(board.validPos([0, 8])).toEqual(false);
+    expect(tk.validPos([0, 8])).toEqual(false);
   });
   test("Array passed in, edge case [-1, 8]", () => {
-    expect(board.validPos([-1, 8])).toEqual(false);
+    expect(tk.validPos([-1, 8])).toEqual(false);
   });
   test("Array passed in, edge case [-1, 7]", () => {
-    expect(board.validPos([-1, 7])).toEqual(false);
+    expect(tk.validPos([-1, 7])).toEqual(false);
   });
   test("Array passed in, edge case [8, 0]", () => {
-    expect(board.validPos([0, 8])).toEqual(false);
+    expect(tk.validPos([0, 8])).toEqual(false);
   });
   test("Array passed in, edge case [8, -1]", () => {
-    expect(board.validPos([-1, 8])).toEqual(false);
+    expect(tk.validPos([-1, 8])).toEqual(false);
   });
   test("Array passed in, edge case [7, -1]", () => {
-    expect(board.validPos([-1, 7])).toEqual(false);
+    expect(tk.validPos([-1, 7])).toEqual(false);
   });
   test("Array passed in, edge case [8, 8]", () => {
-    expect(board.validPos([8, 8])).toEqual(false);
+    expect(tk.validPos([8, 8])).toEqual(false);
   });
   test("Array passed in, edge case [7, 8]", () => {
-    expect(board.validPos([7, 8])).toEqual(false);
+    expect(tk.validPos([7, 8])).toEqual(false);
   });
   test("Array passed in, edge case [8, 7]", () => {
-    expect(board.validPos([8, 7])).toEqual(false);
+    expect(tk.validPos([8, 7])).toEqual(false);
   });
 });
 
